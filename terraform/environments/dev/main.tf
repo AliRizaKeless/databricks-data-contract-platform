11cbe52fef824b111f39ab2afbcbaad918431d0d
Ali Riza Keles <alirizakeles66@gmail.com>
2026-05-21 13:29:26 +0200

    Add Databricks cluster and notebook resources

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -13,3 +13,22 @@ provider "databricks" {
   host  = var.databricks_host
   token = var.databricks_token
 }
+
+# Create a cluster
+
+resource "databricks_cluster" "this" {
+  cluster_name            = "data-contract-cluster"
+  spark_version           = "13.3.x-scala2.12"
+  node_type_id            = "Standard_DS3_v2"
+  autotermination_minutes = 60
+
+  num_workers = 1
+}
+
+# Create a notebook
+
+resource "databricks_notebook" "validation_notebook" {
+  path     = "/Shared/data_contract_validation"
+  language = "PYTHON"
+  source   = "${path.module}/../../../notebooks/databricks_validation.py"
+}
